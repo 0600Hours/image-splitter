2326a422ff415d473af0a1f4216b288c5d424c0b
Thomas' Pendley <tpendley360@gmail.com>
2023-01-24 18:20:58 -0800

    1.0 working

--- split-image.py
+++ split-image.py
@@ -1,15 +1,6 @@
 import cv2;
-import numpy;
+import os;
 import sys;
-
-if (len(sys.argv) != 4):
-  print("usage: split-image.py source hex column\n\tsource: image to split\n\thex: hex code of color to look for\n\tcolumn: how many pixels over from the left to search for the given color")
-  exit()
-src_file, col, hex = sys.argv[1:4]
-col = int(col)
-
-bgr = tuple(int(hex.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))[::-1]
-print(bgr)
 
 def totuple(a):
     try:
@@ -17,9 +8,34 @@ def totuple(a):
     except TypeError:
         return a
 
+# extract args
+if (len(sys.argv) != 4):
+  print("usage: split-image.py source hex column\n\tsource: image to split\n\thex: hex code of color to look for\n\tcolumn: how many pixels over from the left to search for the given color")
+  exit()
+src_file, col, hex = sys.argv[1:4]
+col = int(col)
+
+# make a folder to store split images
+src_prefix, src_type = src_file.split('.')
+folder_name = f'{src_prefix}_splits'
+if not os.path.exists(folder_name):
+  os.mkdir(folder_name)
+
+# convert hex to reversed rgb
+bgr = tuple(int(hex.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))[::-1]
+
+# find pixels
 src = cv2.imread(src_file)
-print(type(src[0, 0]))
-for row in range(src.shape[0]):
+prev = 0
+count = 0
+ht = src.shape[0]
+wt = src.shape[1]
+for row in range(1, ht):
   pxl = src[row, col]
-  if (totuple(pxl) == bgr):
-    print(row)
+  if (row == ht - 1 or totuple(pxl) == bgr):
+    out = os.path.join(folder_name, f'{src_file.rstrip(src_type)}_{count}.{src_type}')
+    cv2.imwrite(out, src[prev:row + 1, 0:wt])
+    count += 1
+    prev = row
+
+print("done");
